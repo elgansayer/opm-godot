074afe1baebc85c444790b6deaf8ca2984052b93
Elgan <elgan.sayer@gmail.com>
2026-02-21 19:07:45 +0000

    Add openmohaa as a fork submodule

--- openmohaa/code/botlib/be_aas_optimize.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef BE_AAS_OPTIMIZE_H
-#define BE_AAS_OPTIMIZE_H
-
-void AAS_Optimize(void);
-
-#endif
--- openmohaa/code/godot/tests/mock_godot_cpp/godot_cpp/variant/dictionary.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef GODOT_DICTIONARY_HPP
-#define GODOT_DICTIONARY_HPP
-
-namespace godot {
-class Dictionary {};
-}
-
-#endif
