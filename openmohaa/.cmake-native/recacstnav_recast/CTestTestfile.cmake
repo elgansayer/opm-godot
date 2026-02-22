64340248a182084b9b3eaeeb2b11ff8babbad9c8
Elgan <elgan.sayer@gmail.com>
2026-02-22 04:56:19 +0000

    gitignore: exclude openmohaa/.cmake-native/ build artifacts

--- openmohaa/.cmake-native/recacstnav_recast/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /run/media/elgan/bdb8dbed-86d7-41d2-90f3-09a45ed5ad9c1/dev/opm-godot/openmohaa/code/thirdparty/recastnavigation/Recast
-# Build directory: /run/media/elgan/bdb8dbed-86d7-41d2-90f3-09a45ed5ad9c1/dev/opm-godot/openmohaa/.cmake-native/recacstnav_recast
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- openmohaa/.cmake-native/recastnav_detour/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /run/media/elgan/bdb8dbed-86d7-41d2-90f3-09a45ed5ad9c1/dev/opm-godot/openmohaa/code/thirdparty/recastnavigation/Detour
-# Build directory: /run/media/elgan/bdb8dbed-86d7-41d2-90f3-09a45ed5ad9c1/dev/opm-godot/openmohaa/.cmake-native/recastnav_detour
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
